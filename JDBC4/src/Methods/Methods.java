04d0eb9f12a794b6e6765091d20fd3dd7d678fb2
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-09-18 08:10:29 -0300

    commitando

--- JDBC4/src/Methods/Methods.java
+++ JDBC4/src/Methods/Methods.java
@@ -7,42 +7,58 @@ import java.sql.SQLException;
 import db.DB;
 
 public class Methods {
-	
+	//nossas connections
 	private Connection conn = null;
 	private PreparedStatement st = null;
 	
+	//construtor
 	public Methods(Connection conn){
 		this.conn = conn;
 	}
 	
+	//atualizando com indice
 	public void Atualize(int IndiceDepartamento) {
+		//tratatando possiveis erros
 	    try {
+	    	//inserindo query que ira atualizat a base de salario
 	        this.st = conn.prepareStatement("UPDATE seller "
 	                + "SET BaseSalary = BaseSalary + ? "
 	                + "WHERE DepartmentId = ?");
+	        //dizendo qual ? irá receber o valor 600.0
 	        this.st.setDouble(1, 600.00);
+	        //dizendo qual ? irá receber o indice
 	        this.st.setInt(2, IndiceDepartamento);
+	        //realizando alterações 
 	        int rowsAffected = st.executeUpdate();
+	        //mostrando linhas afetadas
 	        System.out.println("Done, rows affected: " + rowsAffected);
+	        //capturando erros SQL
 	    } catch (SQLException e) {
 	        e.printStackTrace();
+	        //fechando statement e connections
 	    } finally {
 	        DB.closeStatement(st);
 	        DB.closeConnection();
 	    }
 	}
-
+	
+	//Função para deletar que recebe o indice
 	public void Deletar(int indice) {
+		//tratando provaveis erros
 		   try {
+			   //varivel statement recebe a query que será deletada
 		        this.st = conn.prepareStatement("DELETE FROM department"
 		        		+ " WHERE Id = ?");
+		        //incluindo com set(tipo) oque o ? irá receber
 		        this.st.setInt(1, indice);
+		        //executando as alterações e guardando o retono no nosso row
 			    int row = st.executeUpdate();
-			    
+			    //pritando linhas afetadas
 			    System.out.println("Done! rows Affected: " + row);
-			    
+			    //capturando erro
 			    } catch (SQLException e) {
 			        e.printStackTrace();
+			    //finalizando e fechando Statement e Connection 
 			    } finally {
 			        DB.closeStatement(st);
 			        DB.closeConnection();
--- JDBC4/src/application/Program.java
+++ JDBC4/src/application/Program.java
@@ -8,9 +8,9 @@ import db.DB;
 public class Program {
 	//nosso programa principal onde chamaremos nossas classes de auxilio
 	public static void main(String[] args) {
-	
+		//instanciando connection 
 		Connection conn = DB.getConnection();
-	
+		//usando nossas classes
 		Methods md = new Methods(conn);
 		md.Deletar(5);
 	}
