beb3b72fe3c56ea7282e1649544e2db10b8aa697
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-09-28 13:31:45 -0300

    estudos

---
+++ Topicos_Basicos_Java/src/restrições_convenções_para_nomes/Main.java
@@ -0,0 +1,19 @@
+package restrições_convenções_para_nomes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 89;
+		int y = 60;
+		System.out.println(x & y);
+		//comparando bit a bit com &
+		// 0101 1001 ->comparando bit a bit
+		// 0011 1101 ->0001 1000 (24)
+		System.out.println(x | y);
+		//comparando bit a bit com |
+		// 0101 1001 ->comparando bit a bit
+		// 0011 1101 ->0111 1101 (125)
+	}
+
+}
