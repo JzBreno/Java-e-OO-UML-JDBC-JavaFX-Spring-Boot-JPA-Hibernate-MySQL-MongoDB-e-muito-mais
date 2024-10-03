73455b46d8344b7cf52ce142cc2d31b830c790e0
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-10-03 11:27:47 -0300

    integração wrapper class

--- Topicos_Basicos_Java/src/vetorobjeto/Main.java
+++ Topicos_Basicos_Java/src/vetorobjeto/Main.java
@@ -2,6 +2,7 @@ package vetorobjeto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -12,15 +13,29 @@ public class Main {
 		System.out.println("Digite o tamanho do Vetor");
 		int Numero = scan.nextInt();
 		//inciando nossa list do tipo computador
-		List<Computador> comp = new ArrayList<Computador>() ;
+		ArrayList<Computador> comp = new ArrayList<Computador>() ;
 		//iremos usar a função add inserindo um objeto do tipo computador
 		for (int i=0; i<=Numero;i++) {
 			comp.add(new Computador("xeon","6600","teste","16gb",1));
 		}
 		//percorrendo e printando com indices
-		for(int i=0;i<comp.size();i++) {
-			System.out.println(comp.get(i));
+		//melhor forma de iterar sobre listas
+		for(Computador comps : comp) {
+			System.out.println(comps);
 		}
+		//listas de integer 
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		for(int i=0;i<10;i++) {
+			Random random = new Random();
+			lista.add(random.nextInt(0,101));
+		}
+		
+		for(Integer list : lista) {
+			System.out.println(list);
+		}
+		
+		
+		
 		scan.close();
 
 	}
